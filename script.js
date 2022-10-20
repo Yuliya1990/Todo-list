3c7b26d96e00a5ea84926eeb18b82b2ebdebe4e1
Yulia Solomakha <yulichka700@gmail.com>
2022-10-20 17:12:24 +0200

    With clearing

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 const listContainer = document.querySelector('[data-lists]');
 
 const LOCAL_STORAGE_TASK_KEY = 'list.tasks';
-//let tasks = [];
 let tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASK_KEY)) || ['Add your first task!'];
 
 function render() {
   clearElement(listContainer);
+  console.log(tasks);
+
   tasks.forEach((task) => {
     const taskElement = document.createElement('li');
     taskElement.classList.add('task-name');
     taskElement.innerText = task;
     //add close btn
-    var span = document.createElement('SPAN');
-    var txt = document.createTextNode('\u00D7');
-    span.className = 'close';
-    span.appendChild(txt);
-    taskElement.appendChild(span);
     listContainer.appendChild(taskElement);
   });
 }
@@ -28,23 +24,16 @@ function clearElement(element) {
   }
 }
 
+function delete_from_localStorage(element) {
+  console.log('delete');
+}
+
 function saveAndRender() {
   save();
   render();
 }
 function save() {
   localStorage.setItem(LOCAL_STORAGE_TASK_KEY, JSON.stringify(tasks));
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName('close');
-for (var i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    //localStorage.removeItem()
-    var div = this.parentElement;
-    console.log(div);
-    div.style.display = 'none';
-  };
 }
 
 // Add a "checked" symbol when clicking on a list item
@@ -68,5 +57,11 @@ function newElement() {
   }
   document.getElementById('myInput').value = '';
 
+  saveAndRender();
+}
+
+function clearAll() {
+  localStorage.clear();
+  tasks = [];
   saveAndRender();
 }
